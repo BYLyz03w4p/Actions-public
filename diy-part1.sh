e5d9ceb59f2060b6b2bbcdf6f29015210f3a7819
BYLyz03w4p <lujunxi@msn.com>
2024-12-16 19:49:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,13 @@
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: trainSu
+#=================================================
 
 # Add a feed source
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-# 这个passwall经测试，用不了
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# 大插件包
-# echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-# 小插件包
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# echo 'src-git pkg https://github.com/1774293824/openwrt-pkg' >>feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
